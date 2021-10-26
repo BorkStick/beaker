25f1a5a34c81b11f1e95dab4472d076d3f98a7e6
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-10-26 00:11:25 -0500

    adding springboot

---
+++ beaker-back/src/main/java/host/bork/beakerback/BeakerBackApplication.java
@@ -0,0 +1,13 @@
+package host.bork.beakerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeakerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeakerBackApplication.class, args);
+	}
+
+}
---
+++ beaker-back/src/test/java/host/bork/beakerback/BeakerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package host.bork.beakerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeakerBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
