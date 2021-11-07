aaeeb961a8fab9a0f9836d7e9efcaae79fb89689
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-11-07 16:21:08 -0600

    Front: user add post working

--- beaker-front/src/components/AddUserForm/AddUserForm.jsx
+++ beaker-front/src/components/AddUserForm/AddUserForm.jsx
@@ -1,50 +1,76 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios"
 
-export default function AddUserForm() {
-  return (
-    <div>
-      <form>
-        <div class="form-group">
-          <label>User Name</label>
-          <input
-            type="text"
-            class="form-control rounded-0"
-            placeholder="username"
-            required
-          />
-        </div>
-        <div class="form-group">
-          <label>First Name</label>
-          <input
-            type="text"
-            class="form-control rounded-0"
-            placeholder="Terry"
-            required
-          />
-        </div>
-        <div class="form-group">
-          <label>Last Name</label>
-          <input
-            type="text"
-            class="form-control rounded-0"
-            placeholder="Fromit"
-            required
-          />
-        </div>
-        <div class="form-group">
-          <label>Email</label>
-          <input
-            type="email"
-            class="form-control rounded-0"
-            placeholder="terry.fromit@bork.host"
-            required
-          />
-        </div>
+// post using axios
+// https://www.youtube.com/watch?v=x9UEDRbLhJE
+class AddUserForm extends Component {
+  constructor(props) {
+    super(props);
 
-        <button type="submit" class="btn btn-dark rounded-0">
-          Add
-        </button>
-      </form>
-    </div>
-  );
+    this.state = {
+      userName: "",
+      firstName: "",
+      lastName: "",
+      email: "",
+    };
+  }
+
+  
+
+  changeHandler = (e) => {
+      this.setState({ [e.target.name]: e.target.value })
+  }
+
+  submitHandler = (e) => {
+    //e.preventDefault()
+    
+    
+    console.log(this.state);
+    axios.post('/api/users/add', this.state)
+  }
+
+  render() {
+    const { userName, firstName, lastName, email } = this.state;
+    return (
+      <div>
+        <form onSubmit={this.submitHandler}>
+          <div>
+            <input
+              type="text"
+              name="userName"
+              value={userName}
+              onChange={this.changeHandler}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="firstName"
+              value={firstName}
+              onChange={this.changeHandler}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="lastName"
+              value={lastName}
+              onChange={this.changeHandler}
+            />
+          </div>
+          <div>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={this.changeHandler}
+            />
+          </div>
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    );
+  }
 }
+
+export default AddUserForm;
